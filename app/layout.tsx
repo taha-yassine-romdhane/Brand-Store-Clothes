c754bf223f20d3ec3e1a3e3497f3b99a92e8c412
taha99 <taha.romdhane1999@gmail.com>
2025-01-07 23:15:22 +0100

    try favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,32 @@ import { Inter } from 'next/font/google';
 import { CartProvider } from "@/lib/context/cart-context";
 import Navbar from "@/components/navbar";
 import { Footer } from "@/components/footer";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.lamasette.com/"),
   title: 'LAMASETTE - Timeless Fashion for the Modern Individual',
   description: 'Discover our collection of premium, sustainable clothing crafted for the modern wardrobe.',
   keywords: 'fashion, clothing, sustainable fashion, modern wardrobe, premium clothing, LAMASETTE, Tunisia',
+  icons: {
+    icon: { url: '/favicon.ico', sizes: 'any' }
+  },
   openGraph: {
     title: 'LAMASETTE - Timeless Fashion for the Modern Individual',
     description: 'Discover our collection of premium, sustainable clothing crafted for the modern wardrobe.',
     type: 'website',
     locale: 'ar_TN',
     siteName: 'LAMASETTE',
+    images: [
+      {
+        url: '/favicon.ico',
+        width: 32,
+        height: 32,
+        alt: 'LAMASETTE Favicon',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
